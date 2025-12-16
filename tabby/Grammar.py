167249fc2cc932edcf80b11b960a99e71cd653ee
Samuel Proulx <samuelproulx26@gmail.com>
2025-12-16 00:23:19 -0500

    pixi project

--- tabby/Grammar.py
+++
@@ -1,9 +0,0 @@
-from lark import Lark
-
-GRAMMAR = r"""
-    start: select_stmt  
-    select_stmt: "SELECT" column_list "FROM" table_ref where_clause? join_clause* group_by_clause? agg_clause?
-    column_list: "*" | column ("," column)*
-    column: CNAME
-    table_ref: CNAME
-"""
--- tabby/__init__.py
+++ tabby/__init__.py
@@ -1,33 +1,3 @@
-# # from tabby import dag.dag.DAG, dag.dag.DAGNode
-
-# __all__ = ['dag', 'dataframe']
-
-# from . import dag
-# from . import dataframe
-
-# from .dag import DAG, DAGNode
-# from .dataframe import DataFrame
-
-# from .dag import (
-#     DAG,
-#     DAGNode,
-# )
-
-# from .dataframe import (
-#     DataFrame,
-# )
-
-from .dag import DAG, DAGNode
 from .dataframe import DataFrame
-from .tokentype import TokenType
-from .token import Token
-from .expr import Expr, col
 
-# __all__ = ["DAG", "DAGNode", "DataFrame", "Token", "TokenType"]
-
-# from tabby import dag
-# from tabby.dag import DAG, DAGNode
-# from tabby import token
-# from tabby.token import Token
-# from tabby import tokentype
-# from tabby.tokentype import TokenType
+__all__ = ["DataFrame"]
